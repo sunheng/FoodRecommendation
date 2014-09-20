6763888243f1d41a08416cb48b472dc23013d17d
Jimmy Nguyen <jimmy.nguyen@gatech.edu>
2014-09-20 14:21:52 -0400

    Get recommendations from all pages

--- php/yelp_review_aggregator.php
+++ php/yelp_review_aggregator.php
@@ -2,7 +2,7 @@
 	include_once('simple_html_dom.php');
 	include('menu.php');
 	
-	$numberOfReviews = 121;
+	$numberOfReviews = 10;
 	$url = 'http://www.yelp.com/biz/third-and-vine-jersey-city-2';
 	$url1 = 'http://www.yelp.com/biz/california-pizza-kitchen-atlanta-2';
 	$url2 = 'http://www.yelp.com/biz/bone-garden-cantina-atlanta';
@@ -10,8 +10,6 @@
 	get_recommended_items($url2, $numberOfReviews);
 	
 	function get_recommended_items($url, $numberOfReviews) {
-		// $numberOfReviews /= 40;
-		// echo ceil($numberOfReviews);
 		$html_page = file_get_html($url);
 		$all_reviews = extract_all_comments($html_page);
 		$menu_url = getMenuURL($html_page);
@@ -27,23 +25,26 @@
 			}
 			$comments = strtolower($comments);
 			$index = 0;
-			$recommended_items = '{';
 			foreach($menu_items as $menu_item) {
 				$menu_item = strtolower($menu_item);
 				$numberOfOccurences = substr_count($comments, $menu_item);
 				if($numberOfOccurences != 0) {
-					// if(strlen($recommended_items) > 1) {
-						// $recommended_items = substr($recommended_items, 0, count($recommended_items) - 2);
-						// $recommended_items .= ', "' . $menu_item . '": "' . $numberOfOccurences . '"}';
-					// } else {
-						// $recommended_items .= '"' . $menu_item . '": "' . $numberOfOccurences . '"}';
-					// }
 					$menu_items_occurences[$menu_item] += $numberOfOccurences;
 				}
 			}
-			// echo $recommended_items . '<br>';
 		}
-		print_r($menu_items_occurences);
+		$recommended_items = '{';
+		$key_value = each($menu_items_occurences);
+		while($key_value !== false) {
+			if(strlen($recommended_items) > 1) {
+				$recommended_items = substr($recommended_items, 0, count($recommended_items) - 2);
+				$recommended_items .= ', "' . $key_value[0] . '": "' . $key_value[1] . '"}';
+			} else {
+				$recommended_items .= '"' . $key_value[0] . '": "' . $key_value[1] . '"}';
+			}
+			$key_value = each($menu_items_occurences);
+		}
+		echo $recommended_items . '<br>';
 	}
 	
 	function extract_all_comments($html_page) {
@@ -55,33 +56,38 @@
 		$rating_delimiter = '<i class="star-img';
 		$clipped_comment = "";
 		$clipped_rating = "";
-		$rating = 0;
 		foreach($reviews as $review) {
 			$rating_start_pos = strpos($review, $rating_delimiter);
+			$rating = 0;
 			if ($rating_start_pos !== false) {
 				$unclipped_rating = substr($review, $rating_start_pos);
 				$end_pos = strpos($unclipped_rating, '</i>') + strlen('</i>');
 				$clipped_rating = substr($unclipped_rating, 0, $end_pos);
-				if (strpos($clipped_rating, 'stars_1') !== false) {
-					$rating = 1;
-				} else if (strpos($clipped_rating, 'stars_2') !== false) {
-					$rating = 2;
-				} else if (strpos($clipped_rating, 'stars_3') !== false) {
-					$rating = 3;
-				} else if (strpos($clipped_rating, 'stars_4') !== false) {
+				// if (strpos($clipped_rating, 'stars_1') !== false) {
+					// $rating = 1;
+				// } else if (strpos($clipped_rating, 'stars_2') !== false) {
+					// $rating = 2;
+				// } else if (strpos($clipped_rating, 'stars_3') !== false) {
+					// $rating = 3;
+				// } else 
+				if (strpos($clipped_rating, 'stars_4') !== false) {
 					$rating = 4;
+				} else if (strpos($clipped_rating, 'stars_4_half') !== false) {
+					$rating = 4.5;
 				} else if (strpos($clipped_rating, 'stars_5') !== false) {
 					$rating = 5;
 				}
 			}
-		
-			$start_pos = strpos($review, $comment_delimiter);
-			if ($start_pos !== false) {
-				$unclipped_comment = substr($review, $start_pos);
-				$end_pos = strpos($unclipped_comment, '</p>') + strlen('</p>');
-				$clipped_comment = substr($unclipped_comment, 0, $end_pos);
+			
+			if($rating != 0) {
+				$start_pos = strpos($review, $comment_delimiter);
+				if ($start_pos !== false) {
+					$unclipped_comment = substr($review, $start_pos);
+					$end_pos = strpos($unclipped_comment, '</p>') + strlen('</p>');
+					$clipped_comment = substr($unclipped_comment, 0, $end_pos);
+				}
+				$all_reviews[] = new Review($clipped_comment, $rating);
 			}
-			$all_reviews[] = new Review($clipped_comment, $rating);
 		}
 		unset($all_reviews[0]);
 		return $all_reviews;
